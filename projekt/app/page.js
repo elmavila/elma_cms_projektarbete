bb93ff34eda71c121559117b113587807b1d9713
Elma <vilaelma1@gmail.com>
2025-01-16 15:28:52 +0100

    vercel debugg

--- projekt/app/page.js
+++ projekt/app/page.js
@@ -37,24 +37,6 @@ export default async function Home() {
           Läs mer om mig
         </Link>
       </section>
-
-      {/* <!-- Projektförhandsvisning --> */}
-      {/* <section className="project-preview">
-        <h2>{dataFromFrontPageProjects[0].projectTitle}</h2>
-        <div className="project-cards">
-          {dataFromFrontPageProjects.map((project, index) => (
-            <div className="project-card" key={index}>
-              {project.projectImg?.url ? <Image src={project.projectImg.url} alt={project.projectImg.description} width={500} height={500} /> : <div>Bild saknas</div>}
-              <h3>{project.projectTitle}</h3>
-              <p>{project.projectDescription1}</p>
-              <Link href={`/project/${project.slug}`} className="btn">
-                Se mer
-              </Link>
-            </div>
-          ))}
-        </div>
-      </section> */}
-
       <Footer />
     </>
   )
